a0c775feebf1c8a9a70158a8a9478fb3697b84e9
Lyubosumaz <LyuboslavIlkov@gmail.com>
2022-01-03 23:57:29 +0200

    adding TailwindCSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import Tailwind from './Tailwind.svelte';
     import Todo from './Todo.svelte';
     export let name: string;
 </script>
 
+<Tailwind />
 <main>
     <h1>Hello {name}!</h1>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
